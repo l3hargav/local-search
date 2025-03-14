ff2c2b0b5e18a2727aacaf44669e9f9c072747ba
Rajsekar Balaji <brj3raj@gmail.com>
2025-03-14 12:56:25 +0530

    Added logging

--- inverted_index.py
+++ inverted_index.py
@@ -1,25 +1,33 @@
 import os
+import datetime
+from typing import Dict
 from klepto.archives import dir_archive
 
-path = os.path.expanduser("~/Documents/projects/test_dir/")
+
+# TODO:
+# Add more file types
+# Put in some kind of data structure like a set or list for storing file types
 
 # Get all data from given directory
-def get_data_from_dir(path):
+# Store extracted data for searching
+def get_data_from_dir(path: str) -> None:
+    count = 0
     data = {}
     for root, dirs, files in os.walk(path):
         for file in files:
             name, ext = os.path.splitext(os.path.join(root, file))
-            # TODO:
-            # Add more file types
-            # Put in some kind of data structure like a set or list
             if ext == '.py' or ext == '.txt' or ext == '.rs' or ext == '.c' or ext == '.lua':
                 # Calls file_extractor on each file
                 # And stores the file path and the data extracted in the dictionary
                 data[os.path.join(root, file)] = file_extractor(os.path.join(root, file))
-    return data
+                count += 1
+    with open("logs.txt", 'a') as f:
+        ct = datetime.datetime.now()
+        f.write(f"{ct}: Read {count} files\n")
+    data_archive = dir_archive("data_store", data, cached=False, compression=2)
 
-def file_extractor(file_path):
-    # Get file content
+# Get file content
+def file_extractor(file_path: str) -> str:
     try:
         with open(file_path, 'r') as f:
             content = f.read()
@@ -30,7 +38,7 @@ def file_extractor(file_path):
     return content
 
 
+
 if __name__ == "__main__":
-    data = get_data_from_dir(path)
-    print([key for key in data])
-    print(len(data))
+    path = os.path.expanduser("~/Documents/projects/test_dir/")
+    get_data_from_dir(path)
